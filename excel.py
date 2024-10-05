c51c815f9d70f68ab9487a775c6845297c382be0
honor68 <enochhonour53@gmail.com>
2024-10-05 13:03:01 +0100

    Add files via upload

---
+++ excel.py
@@ -0,0 +1,34 @@
+# def ran_num():
+#     # random = cd.randint(1,8)
+#     return random
+
+# from math import ceil,floor,
+# import math
+# import random
+# import random as ran
+# print(math.pi)
+# print(dir(random))
+# print(floor(ran.random()*10)+ 1)
+
+
+# print(floor(2.5))
+# print(ceil )
+
+# print(ran.randit (1,10))
+
+# names = ["shegun,james,mary,john,amanda"]
+# guess = ran.samples (names,2)
+# print(guess)
+#  print(ran.choice(names,bias))
+
+
+
+
+
+import time as t 
+# print (t.localtime())
+# print (t.strftime("%y-%m,%d %h:%M:%S"))
+
+print ("hello")
+t.sleep(10)
+print ("welcome")
