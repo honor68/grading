e688c8dc2ae150b2dcce478841b8159858a76d11
honor68 <enochhonour53@gmail.com>
2024-11-09 14:24:30 +0100

    Add files via upload

---
+++ first.py
@@ -0,0 +1,53 @@
+# print("pls login")
+# input("register")
+# mygrade = ("correct")
+# mynumber = 16
+# print(mygrade)
+# print(mynumber)
+
+# numbers = 45.9 #float
+# number_two = 56 #integer
+# myName = "Godstime" #string
+# statement = True #booleans
+
+# regnumber = 67.9
+# nomber = 00 
+# contour = 'mystic'
+# countour = "mystic"
+# statement = True
+# statement = False
+ 
+# txt = "hello, and welcome to my world"
+# # capalized_txt = txt.capitalize()
+# # print(capalized_txt)
+
+# txt = "WELCOME TO SCHOOL"
+# lowercase = txt.title()
+# print(txt)
+# print(lowercase)
+# name = "mike"
+# school = "futmina"
+# age = 18
+# print(f"my name is {name} i school in {school} i am {age} old")
+# operations = 240 % 165
+# print(operations)
+
+
+init_var1 = 14
+init_var2 = 47
+
+# # addition
+# additon_result = init_var1 + init_var2
+# print(f"{init_var1} + {init_var2} = {additon_result}")
+
+# # subtraction
+# subtraction_result = init_var1 - init_var2
+# print(f"{init_var1} - {init_var2} = {subtraction_result}")
+
+# # multiplication
+# multiplication_result = init_var1 * init_var2
+# print(f"{init_var1} * {init_var2} = {multiplication_result} ")
+
+# division
+division_result = init_var1 / init_var2
+print(f"{init_var1} / {init_var2} = {division_result}")
